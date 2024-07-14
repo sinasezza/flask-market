15acdcd0e75f5a1c0d0cd8081fc13c8b48a2d95c
sinasezza <s.e.sezza121@gmail.com>
2024-07-14 23:27:35 +0330

    feat(views): market page and view added.

--- app/views.py
+++ app/views.py
@@ -3,5 +3,16 @@ from flask import render_template
 
 
 @app.route("/")
-def hello_world():
+def home_page():
     return render_template("home.html")
+
+
+@app.route("/market/")
+def market_page():
+    items = [
+        {"id": 1, "name": "Chocolate Sandwich Cookies", "barcode": "CCO12345", "description": "Cookie", "price": 0.12},
+        {"id": 2, "name": "Spinach Artichokes", "barcode": "ART45678", "description": "Artichoke", "price": 0.25},
+        {"id": 3, "name": "Lemon Pies", "barcode": "PIP99999", "description": "Lemon Pie", "price": 1.99},
+        {"id": 4, "name": "Chocolate Cake", "barcode": "CAK77777", "description": "Chocolate", "price": 3.45},
+    ]
+    return render_template("market.html", items=items)
